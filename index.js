cdcd026ca1ac34d175cb00bc00e6126f8019167a
edwin karuri njoroge <edwin.njoroge1@student.moringaschool.com>
2024-09-26 23:00:52 -0700

    scopelab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newFavorite) {
+    bestCustomer = newFavorite;
+}
+
+const leastFavoriteCustomer = 'some customer';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'another customer'; // This should throw an error
+}
